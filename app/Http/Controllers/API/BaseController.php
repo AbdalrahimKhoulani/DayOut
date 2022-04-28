66f33d625ab0fab84f2e6a7a08c12f67b6f99716
AbdalrahimKhoulani <abdalrahimkhoulani@gmail.com>
2022-04-28 15:30:50 +0300

    Register function and promotion request

---
+++ app/Http/Controllers/API/BaseController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+
+class BaseController extends Controller
+{
+    public function sendResponse($result, $message)
+    {
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message
+        ];
+
+        return response()->json($response);
+    }
+
+    public function sendError($error, $errorMessage = [], $code = 404)
+    {
+        $response = [
+            'success' => false,
+            'data'=>$error,
+        ];
+
+        if(!empty($errorMessage)){
+            $response['data'] = $errorMessage;
+        }
+
+        return response()->json($response,$code);
+    }
+}
