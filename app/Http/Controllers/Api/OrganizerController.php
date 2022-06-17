58722831df51787f151affc900cf8df8b8e4bd84
AbdalrahimKhoulani <abdalrahimkhoulani@gmail.com>
2022-06-17 11:43:04 +0300

    Fix search trip and Get all organizers and Add search trip

--- app/Http/Controllers/Api/OrganizerController.php
+++ app/Http/Controllers/Api/OrganizerController.php
@@ -156,7 +156,8 @@ class OrganizerController extends BaseController
         $organizerId = $this->getOrganizerId($id);
         if ($organizerId == null) {
             $this->sendErrorToLog('User is not organizer', []);
-            return $this->sendError('User is not organizer', [], 403);
+//            return $this->sendError('User is not organizer', [], 403);
+            return 0;
         }
 
         $trips = Trip::where('organizer_id', $organizerId)->with('customerTrips');
